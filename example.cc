1f207426ec4659cfe72f65f6844d8baa7f588e7e
Sergey Svistunov <sergei-svistunov@yandex.ru>
2019-05-31 16:41:09 +0300

    Add CommandLine example

--- example.cc
+++ example.cc
@@ -1,7 +1,19 @@
 #include <iostream>
 
+#include "base/command_line.h"
+
 int main(int argc, char **argv) {
-    std::cout << "Hello world" << std::endl;
+    base::CommandLine::Init(argc, argv);
+
+    auto *cmd_line = base::CommandLine::ForCurrentProcess();
+
+    for (const auto &sw : cmd_line->GetSwitches()) {
+        std::cout << "Switch " << sw.first << ": " << sw.second << std::endl;
+    }
+
+    for (const auto &arg: cmd_line->GetArgs()) {
+        std::cout << "Arg " << arg << std::endl;
+    }
 
     return 0;
 }
